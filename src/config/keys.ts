6076de3987ec868ac678c4629a7f861348792a38
Alireza Ghavabesh <58913940+Alireza-Ghavabesh@users.noreply.github.com>
2021-10-28 04:11:37 +0330

    Update keys.ts

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -2,6 +2,3 @@ export default {
   mongoURI:
     '---------------PUT YOUR URI HERE :)----------------',
 };
-
-
-// mongodb+srv://oscar:5fnoX6V44f516zJu@cluster0.jct7v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
