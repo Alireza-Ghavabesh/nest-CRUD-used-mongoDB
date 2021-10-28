26bd959426a72574a596f029c618f73d12ff1604
unknown <jacobmarcosoliver@gmail.com>
2021-10-28 04:07:41 +0330

    update 1 for readme.md

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,4 +1,7 @@
 export default {
   mongoURI:
-    'mongodb+srv://oscar:5fnoX6V44f516zJu@cluster0.jct7v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    '---------------PUT YOUR URI HERE :)----------------',
 };
+
+
+// mongodb+srv://oscar:5fnoX6V44f516zJu@cluster0.jct7v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
